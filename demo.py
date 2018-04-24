7ef87059bf3b95b16c0d801344553b1479cdbc87
v_win7 <v_win7@eric.com>
2018-04-24 20:55:32 +0800

    基本功能

--- demo.py
+++ demo.py
@@ -14,26 +14,97 @@
 """
 
 import requests
-proxies = { "http": "192.168.1.163:8888" }
-def pan_list():
-    params = {}
+
+def get_oauth():
+    global_params = {}
+    url ='https://passport.escience.cn/oauth2/authorize?response_type=code&redirect_uri=http://ddl.escience.cn/system/login/token&client_id=87142&theme=full&state=http://ddl.escience.cn/pan/list'
     headers = {   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Encoding': 'gzip, deflate, br',
     'Accept-Language': 'zh-CN,zh;q=0.9',
     'Connection': 'keep-alive',
-    'Host': 'ddl.escience.cn',
+    'Host': 'passport.escience.cn',
     'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) '
-                  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 '
-                  'Safari/537.36'}
-    url = 'http://ddl.escience.cn/pan/list'
-    params.update({'cookies':requests.get(url,headers=headers,proxies=proxies).cookies})
-    return params
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 '
+                  '(KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'}
+    global_params.update({'cookies':requests.get(url,headers=headers,verify=False).cookies})
+    return global_params
 
-def system_login():
-    params = pan_list()
-    cookies = params['cookies']
-    url = 'http://ddl.escience.cn/system/login?&vwb.requesturl=http%3A%2F%2Fddl.escience.cn%2Fpan%2Flist'
+def oauth():
+    global_params = get_oauth()
+    cookies = global_params['cookies']
+    headers = {   'Accept': 'application/json, text/javascript, */*; q=0.01',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'zh-CN,zh;q=0.9',
+    'Connection': 'keep-alive',
+    'Content-Length': '140',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'Host': 'passport.escience.cn',
+    'Origin': 'https://passport.escience.cn',
+    'Referer': 'https://passport.escience.cn/oauth2/authorize?response_type=code&redirect_uri=http://ddl.escience.cn/system/login/token&client_id=87142&theme=full&state=http://ddl.escience.cn/pan/list',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 '
+                  '(KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36',
+    'X-Requested-With': 'XMLHttpRequest'}
+    url = 'https://passport.escience.cn/oauth2/authorize'
+    data = {   'clientId': '87142',
+                'clientName': '团队文档库',
+                'pageinfo': 'checkPassword',
+                'password': 'Aaa00123',
+                'userName': '761701732@qq.com'}
+    requests.post(url,headers=headers,cookies=cookies,verify=False,data=data)
+    return global_params
+
+def oauth_1():
+    global_params = oauth()
+    cookies = global_params['cookies']
+    url = 'https://passport.escience.cn/oauth2/authorize?client_id=87142&redirect_uri=http://' \
+          'ddl.escience.cn/system/login/token&response_type=code&state=http://ddl.escience.cn/pan/list&theme=full'
+    headers ={   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'zh-CN,zh;q=0.9',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    'Content-Length': '87',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Host': 'passport.escience.cn',
+    'Origin': 'https://passport.escience.cn',
+    'Referer': 'https://passport.escience.cn/oauth2/authorize?response_type=code&redirect_uri=http://ddl.escience.cn/system/login/token&client_id=87142&theme=full&state=http://ddl.escience.cn/pan/list',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 '
+                  '(KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'}
+    data ={   'act': 'Validate',
+    'pageinfo': 'userinfo',
+    'password': 'Aaa00123',
+    'theme': 'full',
+    'userName': '761701732@qq.com'}
+    r = requests.post(url,headers=headers,data=data,verify=False,cookies=cookies,allow_redirects=False)
+    r_location = r.headers['Location']
+    global_params.update({'url_location':r_location})
+    return global_params
+
+def location():
+    global_params = oauth_1()
+    location_url = global_params['url_location']
+    cookies = global_params['cookies']
+    headers = {   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'zh-CN,zh;q=0.9',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    'Host': 'ddl.escience.cn',
+    'Origin': 'https://passport.escience.cn',
+    'Referer': 'https://passport.escience.cn/oauth2/authorize?response_type=code&redirect_uri=http://ddl.escience.cn/system/login/token&client_id=87142&theme=full&state=http://ddl.escience.cn/pan/list',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 '
+                  '(KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'}
+    r = requests.get(url=location_url,headers=headers,cookies=cookies,allow_redirects=False)
+    global_params.update({'cookies':r.cookies})
+    return global_params
+
+
+def get_file():
+    global_params = location()
+    cookies = global_params['cookies']
+    url = 'http://ddl.escience.cn/pan/download?path=%2FLinux_Chales_crack_4.2.5.tar.gz'
     headers = {   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
     'Accept-Encoding': 'gzip, deflate',
     'Accept-Language': 'zh-CN,zh;q=0.9',
@@ -41,9 +112,17 @@ def system_login():
     'Host': 'ddl.escience.cn',
     'Referer': 'http://ddl.escience.cn/pan/list',
     'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) '
-                  'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 '
-                  'Safari/537.36'}
-    r = requests.get(url,headers=headers,cookies=cookies,proxies=proxies)
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 '
+                  '(KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'}
 
-system_login()
+    res = requests.get(url,headers=headers,verify=False,cookies=cookies)
+    res.raise_for_status()
+    playFile = open('Linux_Chales_crack_4.2.5.tar.gz', 'wb')
+    for chunk in res.iter_content(100000):
+        playFile.write(chunk)
+    playFile.close()
+
+
+get_file()
+
+
--- utiles/params_formate.py
+++ utiles/params_formate.py
@@ -13,29 +13,47 @@
 @time: 18/4/24 下午1:21
 """
 
-import pprint
-
+import pprint,urllib3
+from urllib.parse import unquote
 str = '''
 
-Host: ddl.escience.cn
-Connection: keep-alive
-Upgrade-Insecure-Requests: 1
-User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36
+User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36
+Accept-Encoding: gzip, deflate, br
 Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8
-Referer: http://ddl.escience.cn/pan/list
-Accept-Encoding: gzip, deflate
+Connection: keep-alive
 Accept-Language: zh-CN,zh;q=0.9
-Cookie: JSESSIONID=DBB62FD55B2D5F163F24297740056FC3-n1
+Cache-Control: max-age=0
+Host: ddl.escience.cn
+Origin: https://passport.escience.cn
+Referer: https://passport.escience.cn/oauth2/authorize?response_type=code&redirect_uri=http://ddl.escience.cn/system/login/token&client_id=87142&theme=full&state=http://ddl.escience.cn/pan/list
+Upgrade-Insecure-Requests: 1
+Cookie: JSESSIONID=2DD7CEAF782F66C6905615EBD968606F-n1; AUTO_FILL="761701732@qq.com"
 
 '''
-
-
-str = str.split('\n')
-str = [i for i in str if len(i)>0]
 params_dict = {}
-for i in str :
-    m = i.split(': ')
-    params_dict.update({m[0]:m[1]})
-pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(params_dict)
+if ':'  in str:
+    str = str.split('\n')
+    str = [i for i in str if len(i)>0]
+
+    for i in str :
+        m = i.split(': ')
+        params_dict.update({m[0]:m[1]})
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(params_dict)
+
+else:
+    str = unquote(str).strip()
+    str = str.split('&')
+    for i in str:
+        m = i.split('=')
+        params_dict.update({m[0]:m[1]})
+
+
+    #     for h in i:
+    #         n = h.split('=')
+    #         params_dict.update({n[0]:n[1]})
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(params_dict)
+
+
 
