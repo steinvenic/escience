ef4549c388c5cfc29f7298f5d32e7a86b01e6965
LXX <lxx@foo.com>
2018-04-26 23:31:38 +0800

    ss

--- tmp.py
+++ tmp.py
@@ -9,8 +9,14 @@
 @file: tmp.py
 @time: 2018/4/22 3:34
 """
-import requests
-import sys,configparser,pprint
-pp = pprint.PrettyPrinter(indent=4)
-pp.pprint({'Host': 'ddl.escience.cn', 'Connection': 'keep-alive', 'Content-Length': '47', 'Accept': 'application/json, text/javascript', 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8', 'Referer': 'http://ddl.escience.cn/pan/list'})
-
+from prettytable import PrettyTable
+x = PrettyTable()
+x.field_names = ["City name", "Area", "Population", "Annual Rainfall"]
+x.add_row(["Adelaide",1295, 1158259, 600.5])
+x.add_row(["Brisbane",5905, 1857594, 1146.4])
+x.add_row(["Darwin", 112, 120900, 1714.7])
+x.add_row(["Hobart", 1357, 205556, 619.5])
+x.add_row(["Sydney", 2058, 4336374, 1214.8])
+x.add_row(["Melbourne", 1566, 3806092, 646.9])
+x.add_row(["Perth", 5386, 1554769, 869.4])
+print(x)
